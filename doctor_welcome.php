ef8ca3cb44f466225944494f1dff3bb0b0977ec4
tamaykaragulle <83011425+tamaykaragulle@users.noreply.github.com>
2022-07-07 11:20:11 +0300

    .

--- doctor_welcome.php
+++ doctor_welcome.php
@@ -1,17 +1,31 @@
+<?php
+require_once "config.php";
+session_start();
+$doctor_username = $_SESSION['doctor_username'];
+$result = $link->query("SELECT doctor_profile_photo FROM doctors WHERE doctor_username = '$doctor_username'");
+//$sql = "SELECT * FROM products WHERE id = $id";
+//$sth = $db->query($sql);
+$resultt=mysqli_fetch_array($result);
+echo '<img class="profile-img" src="data:image/jpeg;base64,'.base64_encode( $resultt['doctor_profile_photo'] ).'"/>';
+ ?>
 <!DOCTYPE html>
 <html lang="tr">
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" href="style.css">
   </head>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   <body>
     <?php
-      session_start();
-      $doctor_username = $_SESSION['doctor_username'];
       echo "Welcome $doctor_username";
       echo "<br>";
     ?>
+    <form action="upload_profile_photo.php" method="post" enctype="multipart/form-data">
+        <label>Select Image File:</label>
+        <input type="file" name="image">
+        <input type="submit" name="submit" value="Upload">
+    </form>
     <a class="button" href="edit_doctor_profile.php">Edit your profile</a>
     <br />
     <a class="button" href="logout.php">Logout</a>
